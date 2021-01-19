b7d19ebc2f68ad80de8c961d7e44565e61838fb3
kairat <ltdcvet@gmail.com>
2021-01-19 21:14:27 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
-let tasks = [
-  'Сходить в магазин',
-  'Приготовить обед',
-  'Поесть',
-  'Отдохнуть',
-  'Работать'
+let users = [
+  {
+    firstName: 'Mike',
+    lastName: 'Wazowski',
+    login: '#wazowski',
+    followers: '50',
+  },
+  {
+    firstName: 'Sam',
+    lastName: 'Burner',
+    login: '#burner',
+    followers: '25',
+  },
+  {
+    firstName: 'Vasya',
+    lastName: 'Pupkin',
+    login: '#pupkin',
+    followers: '500',
+  },
+  {
+    firstName: 'Jack',
+    lastName: 'London',
+    login: '#london',
+    followers: '115',
+  },
+  {
+    firstName: 'Brad',
+    lastName: 'Pitt',
+    login: '#pitt',
+    followers: '5',
+  },
 ];
 
-for(let item of tasks) {
-  let task = $('<li>');
-  task.text(item);
-  $('.todo').append(task);
+for(let item of users) {
+  let user = $('<li>');
+  // let login = $("<a href=" + "item.login + >"item.login  + "</a>");
+  console.log(login);
+  user.text(  `${item.firstName} ${item.lastName}`  );
+  user.append(login);
+  $('.twitter__list').append(user);
 }
